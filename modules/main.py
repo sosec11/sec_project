289ae7e45c09d5ebf5f30befb3f0df354ad959c3
sosec11 <S.o.sec@outlook.com>
2026-05-03 23:59:41 +0200

    feat: add main entry point to run log analyzer

--- modules/main.py
+++ modules/main.py
@@ -1,5 +1,17 @@
-def main():
-    print("Log Analyzer started...")
+from modules.log_analyzer.parser import read_log_file
 
-    if __name__ == "__main__":
-        main()
+
+def main():
+    log_file = "data/sample_logs.log"
+
+    lines = read_log_file(log_file)
+
+    print("Log Analyzer started")
+    print(f"Number of log lines: {len(lines)}")
+
+    for line in lines:
+        print(line.strip())
+
+
+if __name__ == "__main__":
+    main()
