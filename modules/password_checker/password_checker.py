a0b7d6c1a0f36fe21a086e533e5bd4ee314bca06
sosec11 <S.o.sec@outlook.com>
2026-05-07 23:59:31 +0200

    feat: add password parameters

--- modules/password_checker/password_checker.py
+++ modules/password_checker/password_checker.py
@@ -1,3 +1,12 @@
 def check_password_strength(password):
     score = 0
     feedback = []
+
+    if len(password) >= 8:
+        score += 1
+    else:
+        feedback.append("Le mot de passe est trop court (min 8 caractères)")
+    if any(char.islower() for char in password):
+        score += 1
+    else:
+        feedback.append("Ajoute des lettres minuscules")
