36d84040412c7834cab0aab9c331d17f1a29a51b
sosec11 <S.o.sec@outlook.com>
2026-05-30 22:05:21 +0200

    Add CLI utility functions and improve user interface

--- modules/log_analyzer/detector.py
+++ modules/log_analyzer/detector.py
@@ -1,5 +1,5 @@
 from .patterns import SUSPICIOUS_KEYWORDS
-from utils.display import print
+from utils.display import print_warning
 
 def detect_suspicious_lines(logs):
     suspicious_count = 0
@@ -7,11 +7,11 @@ def detect_suspicious_lines(logs):
     for line in logs:
         for keyword, severity in SUSPICIOUS_KEYWORDS.items():
             if keyword.lower() in line.lower():
-                suspicious_count += 1
-                print("[!] Suspicious activity found:")
-                print("SEVERITY: {severity}git ")
-                print(line)
-                break
 
-    print(f"\nAnalysis completed.")
-    print(f"Suspicious events detected: {suspicious_count}")
+                suspicious_count += 1
+
+                print_warning("[!] Suspicious activity found:")
+                print(f"SEVERITY: {severity}")
+                print(line)
+
+                break
--- utils/display.py
+++ utils/display.py
@@ -7,7 +7,7 @@ def print_menu():
     print("1. Log Analyzer")
     print("2. Password Checker")
 
-    def print_warning(message):
+def print_warning(message):
 
     print(f"\033[91m{message}\033[0m")
 
