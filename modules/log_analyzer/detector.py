1132b8923382704cf35da5fd492f16be5520602f
sosec11 <S.o.sec@outlook.com>
2026-05-31 23:14:52 +0200

    Add timestamp to suspicious event detection

--- modules/log_analyzer/detector.py
+++ modules/log_analyzer/detector.py
@@ -1,5 +1,6 @@
 from .patterns import SUSPICIOUS_KEYWORDS
 from utils.display import print_warning
+from data import datetime
 
 def detect_suspicious_lines(logs):
     suspicious_count = 0
@@ -9,7 +10,8 @@ def detect_suspicious_lines(logs):
             if keyword.lower() in line.lower():
 
                 suspicious_count += 1
-
+                current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                print(f"Detection time: {current_time}")
                 print_warning("[!] Suspicious activity found:")
                 print(f"SEVERITY: {severity}")
                 print(line)
