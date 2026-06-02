65fcfcf28d303ab1f0fcc6ca51461dd9f02efe5b
sosec11 <S.o.sec@outlook.com>
2026-06-02 12:54:48 +0200

    Add global risk level for log analysis

--- modules/log_analyzer/detector.py
+++ modules/log_analyzer/detector.py
@@ -3,17 +3,22 @@ from utils.display import print_warning
 from datetime import datetime
 
 def detect_suspicious_lines(logs):
-    suspicious_count = 0
+    if suspicious_count == 0:
+        print("Risk level: LOW")
+    elif suspicious_count <= 3:
+        print("Risk level: MEDIUM")
+    else:
+        print("Risk level: HIGH")
 
-    for line in logs:
-        for keyword, severity in SUSPICIOUS_KEYWORDS.items():
-            if keyword.lower() in line.lower():
+        for line in logs:
+            for keyword, severity in SUSPICIOUS_KEYWORDS.items():
+                if keyword.lower() in line.lower():
 
-                suspicious_count += 1
-                current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-                print(f"Detection time: {current_time}")
-                print_warning("[!] Suspicious activity found:")
-                print(f"SEVERITY: {severity}")
-                print(line)
+                    suspicious_count += 1
+                    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                    print(f"Detection time: {current_time}")
+                    print_warning("[!] Suspicious activity found:")
+                    print(f"SEVERITY: {severity}")
+                    print(line)
 
-                break
+                    break
