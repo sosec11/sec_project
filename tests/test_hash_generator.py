15bb49b2088f893f58c16ed45cc3fb773c7e1a21
sosec11 <S.o.sec@outlook.com>
2026-06-17 23:19:25 +0200

    Add sha256 hash for hash generator test

--- tests/test_hash_generator.py
+++ tests/test_hash_generator.py
@@ -4,4 +4,6 @@ md5_hash, sha256_hash = generate_hashes("hello")
 
 assert md5_hash == "5d41402abc4b2a76b9719d911017c592"
 
+assert sha256_hash == "2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824"
+
 print("Hash test passed")
